22fb69059b6a5e04a45194a098023d8151e52ae8
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-11 12:02:19 +0700

    add vitest and setupFiles

---
+++ src/__test__/setup.ts
@@ -0,0 +1,10 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// contoh penggunaan setupFiles
+// - menyiapkan mock
+// - inisialisasi variabel global di banyak tes
+// - konfigurasi environment khusus untuk pengujian
