b21dd1f19d380cf13e8c0223200bcd294ecb8b47
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-12 20:28:28 +0700

    refactor Register to enable new user regist

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,26 +1,39 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { registerUser } from '../slices/userSlice';
+import { AppDispatch } from '../app/store';
 
 export default function Register() {
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isHovered, setIsHovered] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const registerHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      dispatch(registerUser({ name: fullName, username, password })).unwrap();
+      alert('Registration successful!');
+      navigate('/login');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="flex flex-col w-full max-w-xl mx-auto min-h-[15rem] justify-center items-center gap-8 p-4">
       <div className="flex flex-row items-center justify-start gap-2 w-full">
-        <a className="text-blue-700" href="github.com" target="_blank">
-          <span className="text-lg hover:font-semibold">login?</span>
-        </a>
-        <a className="text-blue-700" href="github.com" target="_blank">
-          <span className="text-lg hover:font-semibold">register?</span>
-        </a>
+        <Link to="/login" className="text-blue-700">
+          <span className="text-lg hover:font-semibold">
+            already have account?
+          </span>
+        </Link>
       </div>
 
-      <form
-        className="flex flex-col w-[70%] gap-3"
-        onSubmit={(e) => {
-          e.preventDefault();
-          alert('submitted');
-        }}
-      >
+      <form className="flex flex-col w-[70%] gap-3" onSubmit={registerHandler}>
         <label
           htmlFor="mewhen-name"
           className="font-semibold flex flex-row gap-2 group"
@@ -29,6 +42,8 @@ export default function Register() {
           <input
             type="text"
             id="mewhen-name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
             className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1 outline-green-600' : 'outline-none'}`}
             required
           />
@@ -41,6 +56,8 @@ export default function Register() {
           <input
             type="text"
             id="mewhen-username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1  outline-green-600' : 'outline-none'}`}
             required
           />
@@ -53,6 +70,8 @@ export default function Register() {
           <input
             type="password"
             id="mewhen-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1  outline-green-600' : 'outline-none'}`}
             required
           />
