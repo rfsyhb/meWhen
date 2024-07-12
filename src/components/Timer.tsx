6ff43d10b009d25558ba7bd382fd76ed5d9cb5ca
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-12 17:38:59 +0700

    refactor Timer to match Login

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,50 +1,54 @@
-import { useState } from 'react';
 import { FaRegFrownOpen } from 'react-icons/fa';
+import { AppDispatch, RootState } from '../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import NavigationButtons from './NavigationButtons';
+import { useEffect, useState } from 'react';
+import { getUserProfile, logout } from '../slices/userSlice';
 
 export default function Timer() {
-  const [isAuthed, setIsAuthed] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const [isAddTodo, setIsAddTodo] = useState(false);
+  const [NewTodo, setNewTodo] = useState('');
+  const user = useSelector((state: RootState) => state.user.user);
+  const isAuthed = user ? true : false;
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(getUserProfile());
+    }
+  }, [dispatch]);
+
+  const logoutHandler = () => dispatch(logout());
+
   return (
     <div className="flex flex-col w-full max-w-4xl mx-auto min-h-[15rem] justify-center items-center gap-2 p-4">
       <div className="flex flex-col sm:flex-row w-full justify-between px-6 gap-4 sm:gap-0">
         <div className="flex flex-row items-center gap-2">
           {isAuthed ? (
             <>
-              <p className="text-xl font-semibold">Hello Rafi Syihab!</p>
-              <a className="text-blue-700" href="github.com" target="_blank">
+              <p className="text-xl font-semibold">Hello {user!.name}</p>
+              <button
+                type="button"
+                className="text-blue-700 hover:font-semibold"
+                onClick={logoutHandler}
+              >
                 logout?
-              </a>
+              </button>
             </>
           ) : (
             <>
-              <a className="text-blue-700" href="github.com" target="_blank">
+              <Link to="/login" className="text-blue-700">
                 <span className="text-lg hover:font-semibold">login?</span>
-              </a>
-              <a className="text-blue-700" href="github.com" target="_blank">
+              </Link>
+              <Link to="/register" className="text-blue-700">
                 <span className="text-lg hover:font-semibold">register?</span>
-              </a>
+              </Link>
             </>
           )}
         </div>
-        <div className="flex flex-row gap-3">
-          <button
-            className="bg-text text-white font-semibold px-4 rounded-xl border-2 border-black"
-            onClick={() => setIsAuthed(!isAuthed)}
-          >
-            Main
-          </button>
-          <button
-            className="font-semibold px-4 bg-bgColor rounded-xl border-2 border-black hover:bg-text hover:text-white hover:border-black"
-            onClick={() => alert('Setting Clicked!')}
-          >
-            Setting
-          </button>
-          <button
-            className="font-semibold px-4 py-1 bg-bgColor rounded-xl border-2 border-black hover:bg-text hover:text-white hover:border-black"
-            onClick={() => alert('History Clicked!')}
-          >
-            History
-          </button>
-        </div>
+        <NavigationButtons />
       </div>
       <div className="flex flex-col md:flex-row w-full h-auto md:h-48 justify-center gap-4">
         <div className="flex flex-col md:flex-row w-full md:w-[50%] justify-center items-center rounded-xl gap-3 md:gap-8 bg-cardMain shadow-md p-4">
@@ -66,8 +70,34 @@ export default function Timer() {
         </div>
         <div className="flex flex-col w-full md:w-[50%] rounded-xl bg-cardMain shadow-md px-4">
           <div className="flex flex-row justify-between items-center px-4 pt-2 pb-1 border-b border-black">
-            <p className="text-sm">10 July 2024</p>
-            <button onClick={() => alert('added')}>+add</button>
+            {isAddTodo && isAuthed ? (
+              <div className="flex items-center w-full">
+                <input
+                  type="text"
+                  className="flex-grow bg-transparent focus:outline-none"
+                  placeholder="new todo?"
+                  value={NewTodo}
+                  onChange={(e) => setNewTodo(e.target.value)}
+                />
+                <button
+                  onClick={() => setIsAddTodo(false)}
+                  className="pl-2 hover:font-semibold"
+                >
+                  +add new
+                </button>
+              </div>
+            ) : (
+              <>
+                <p className="text-sm">10 July 2024</p>
+                <button
+                  onClick={() => setIsAddTodo(true)}
+                  className="pl-2 hover:font-semibold"
+                  disabled={!isAuthed}
+                >
+                  {isAuthed ? '+add new' : 'login needed!'}
+                </button>
+              </>
+            )}
           </div>
           <div className="flex flex-col justify-center items-center h-full">
             {isAuthed ? (
