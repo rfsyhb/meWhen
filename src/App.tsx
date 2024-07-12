1cdae1eb2874513c214f0a4d87f39c43057f1f9f
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-12 15:38:28 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Setting from './components/Setting';
 import Timer from './components/Timer';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
-  // justify-center untuk x-axis, items-center untuk y-axis
   return (
     <div className="flex justify-center items-center h-screen text-text">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Timer />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
