98cac02f4022c3bb228c63469648e63001c94350
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-12 17:38:26 +0700

    refactor Login comp

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
+import { loginUser } from '../slices/userSlice';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../app/store';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isHovered, setIsHovered] = useState(false);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await dispatch(loginUser({ username, password })).unwrap();
+      alert('Login successful!');
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="flex flex-col w-full max-w-xl mx-auto min-h-[15rem] justify-center items-center gap-8 p-4">
@@ -14,7 +33,7 @@ export default function Login() {
         </a>
       </div>
 
-      <form className="flex flex-col w-[70%] gap-3">
+      <form className="flex flex-col w-[70%] gap-3" onSubmit={handleLogin}>
         <label
           htmlFor="mewhen-username"
           className="font-semibold flex flex-row gap-2 group"
@@ -23,6 +42,8 @@ export default function Login() {
           <input
             type="text"
             id="mewhen-username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1 outline-green-600' : 'outline-none'}`}
             required
           />
@@ -35,6 +56,8 @@ export default function Login() {
           <input
             type="password"
             id="mewhen-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1 outline-green-600' : 'outline-none'}`}
             required
           />
