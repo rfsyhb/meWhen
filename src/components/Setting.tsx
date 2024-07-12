283d3101ea3f12716ef5c284d48cac767a56449f
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-12 21:46:47 +0700

    integrate Setting with settingSlice

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -1,30 +1,61 @@
-import { useState } from 'react';
+import NavigationButtons from './NavigationButtons';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store';
+import { useSelector } from 'react-redux';
+import {
+  setMainWebhook,
+  setReminderWebhook,
+  setReminderUserId,
+  setDiscordEnabled,
+  setTimerTitle,
+  setTimerDuration,
+} from '../slices/settingSlice.ts';
 
 export default function Setting() {
-  const [discordEnabled, setDiscordEnabled] = useState(true);
+  const dispatch = useDispatch<AppDispatch>();
+  const setting = useSelector((state: RootState) => state.setting);
+
+  const handleMainwebhookChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setMainWebhook(e.target.value));
+  };
+
+  const handleReminderWebhookChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch(setReminderWebhook(e.target.value));
+  };
+
+  const handleReminderUserIdChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch(setReminderUserId(e.target.value));
+  };
+
+  const handleDiscordEnabledChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch(setDiscordEnabled(e.target.checked));
+  };
+
+  const handleTimerTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setTimerTitle(e.target.value));
+  };
+
+  const handleTimerDurationChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = parseInt(e.target.value);
+    if (value > -1 && value <= 60) {
+      dispatch(setTimerDuration(value));
+    }
+  };
+
+  const location = useLocation();
 
   return (
     <div className="flex flex-col w-full max-w-xl mx-auto min-h-[15rem] justify-center items-center gap-2 p-4">
-      <div className="flex flex-row gap-3">
-        <button
-          className="bg-text text-white font-semibold px-4 rounded-xl border-2 border-black"
-          onClick={() => alert('Main Clicked!')}
-        >
-          Main
-        </button>
-        <button
-          className="font-semibold px-4 bg-bgColor rounded-xl border-2 border-black hover:bg-text hover:text-white hover:border-black"
-          onClick={() => alert('Setting Clicked!')}
-        >
-          Setting
-        </button>
-        <button
-          className="font-semibold px-4 py-1 bg-bgColor rounded-xl border-2 border-black hover:bg-text hover:text-white hover:border-black"
-          onClick={() => alert('History Clicked!')}
-        >
-          History
-        </button>
-      </div>
+      <NavigationButtons location={location.pathname} />
       <div className="w-full">
         <div className="flex flex-col gap-4 bg-cardMain p-6 rounded-xl border border-black border-opacity-20">
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between">
@@ -50,16 +81,16 @@ export default function Setting() {
                   <input
                     id="enableDiscord"
                     type="checkbox"
-                    checked={discordEnabled}
-                    onChange={() => setDiscordEnabled(!discordEnabled)}
+                    checked={setting.isDiscordEnabled}
+                    onChange={handleDiscordEnabledChange}
                     className="sr-only"
                   />
                   <div
-                    className={`block w-8 h-4 rounded-full ${discordEnabled ? 'bg-black' : 'bg-gray-400'}`}
+                    className={`block w-8 h-4 rounded-full ${setting.isDiscordEnabled ? 'bg-black' : 'bg-gray-400'}`}
                   />
                   <div
                     className={`dot absolute left-0 top-0 bg-white w-4 h-4 border border-gray-400 rounded-full transition ${
-                      discordEnabled ? 'transform translate-x-4' : ''
+                      setting.isDiscordEnabled ? 'transform translate-x-4' : ''
                     }`}
                   />
                 </div>
@@ -69,33 +100,33 @@ export default function Setting() {
                   <input
                     id="webhook"
                     type="text"
-                    value="https://discord.com/api/webhooks/1234567890/abcdefg"
-                    onChange={() => alert('Webhook Clicked!')}
+                    value={setting.mainWebhook}
+                    onChange={handleMainwebhookChange}
                     className="rounded-md px-2 w-16"
                     placeholder="main"
-                    disabled={!discordEnabled}
+                    disabled={!setting.isDiscordEnabled}
                   />
                 </label>
                 <label htmlFor="webhookReminder">
                   <input
                     id="webhookReminder"
                     type="text"
-                    value="https://discord.com/api/webhooks/1234567890/abcdefg"
-                    onChange={() => alert('Webhook Reminder Clicked!')}
+                    value={setting.reminderWebhook}
+                    onChange={handleReminderWebhookChange}
                     className="rounded-md px-2 w-16"
                     placeholder="spam"
-                    disabled={!discordEnabled}
+                    disabled={!setting.isDiscordEnabled}
                   />
                 </label>
                 <label htmlFor="usernameReminder">
                   <input
                     id="usernameReminder"
                     type="text"
-                    value="1234567890"
-                    onChange={() => alert('Username Reminder Clicked!')}
+                    value={setting.reminderUserId}
+                    onChange={handleReminderUserIdChange}
                     className="rounded-md px-2 w-16"
                     placeholder="UID"
-                    disabled={!discordEnabled}
+                    disabled={!setting.isDiscordEnabled}
                   />
                 </label>
               </div>
@@ -108,11 +139,11 @@ export default function Setting() {
               <input
                 id="title"
                 type="text"
-                value="main"
-                onChange={() => alert('Title Clicked!')}
+                value={setting.timerTitle}
+                onChange={handleTimerTitleChange}
                 className="rounded-md px-2"
                 placeholder="berak"
-                // maxLength="25"
+                maxLength={25}
               />
             </label>
             <label
@@ -123,9 +154,11 @@ export default function Setting() {
               <input
                 id="duration"
                 type="number"
-                value="25"
-                onChange={() => alert('Duration Clicked!')}
+                value={setting.timerDuration}
+                onChange={handleTimerDurationChange}
                 className="rounded-md px-2"
+                min={0}
+                max={600}
               />
             </label>
           </div>
