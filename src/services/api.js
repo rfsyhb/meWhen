ce91efdebb1f9433d1d2fa7f9dd1de9687fad6e9
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-13 12:26:44 +0700

    update BASE_URL to deployed BE using Vercel

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
  */
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://me-when-be.vercel.app/';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
