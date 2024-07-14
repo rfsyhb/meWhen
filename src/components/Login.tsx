23250bfa2bf41fffcaf5b95d87b03247a9b2e76f
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-14 21:58:40 +0700

    refactor Login component to use custom hooks and reusable components

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
 import { loginUser } from '../slices/userSlice';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../app/store';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import LinkNavigation from './common/LinkNavigation';
+import AuthForm from './common/AuthForm';
+import InputField from './common/InputField';
+import useInput from '../hooks/useInput';
 
 export default function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isHovered, setIsHovered] = useState(false);
+  const [username, onUsernameChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -24,53 +26,29 @@ export default function Login() {
 
   return (
     <div className="flex flex-col w-full max-w-xl mx-auto min-h-[15rem] justify-center items-center gap-8 p-4">
-      <div className="flex flex-row items-center justify-start gap-2 w-full">
-        <Link to="/" className="text-blue-700">
-          <span className="text-lg hover:font-semibold">home?</span>
-        </Link>
-        <Link to="/register" className="text-blue-700">
-          <span className="text-lg hover:font-semibold">register?</span>
-        </Link>
-      </div>
+      <LinkNavigation
+        links={[
+          { path: '/', label: 'home?' },
+          { path: '/register', label: 'register?' },
+        ]}
+      />
 
-      <form className="flex flex-col w-[70%] gap-3" onSubmit={handleLogin}>
-        <label
-          htmlFor="mewhen-username"
-          className="font-semibold flex flex-row gap-2 group"
-        >
-          <span className="w-[30%] group-hover:translate-x-3">username</span>
-          <input
-            type="text"
-            id="mewhen-username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1 outline-green-600' : 'outline-none'}`}
-            required
-          />
-        </label>
-        <label
-          htmlFor="mewhen-password"
-          className="font-semibold flex flex-row gap-2 group"
-        >
-          <span className="w-[30%] group-hover:translate-x-3">password</span>
-          <input
-            type="password"
-            id="mewhen-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={`w-[70%] rounded-md bg-cardMain shadow-sm p-1 border border-black border-opacity-20 font-normal group-hover:border-opacity-70 ${isHovered ? 'outline outline-1 outline-green-600' : 'outline-none'}`}
-            required
-          />
-        </label>
-        <button
-          type="submit"
-          className="rounded-lg shadow-sm hover:shadow-md border border-black"
-          onMouseOver={() => setIsHovered(true)}
-          onMouseOut={() => setIsHovered(false)}
-        >
-          <span className="flex justify-end p-1 pr-3">login</span>
-        </button>
-      </form>
+      <AuthForm onSubmit={handleLogin} formUsage="login">
+        <InputField
+          id="mewhen-username"
+          label="username"
+          type="text"
+          value={username}
+          onChange={onUsernameChange}
+        />
+        <InputField
+          id="mewhen-password"
+          label="password"
+          onChange={onPasswordChange}
+          type="password"
+          value={password}
+        />
+      </AuthForm>
     </div>
   );
 }
