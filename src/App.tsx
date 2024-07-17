a22f4a593c70c31a87537cc8f4e7cf9831fd3feb
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-17 14:27:44 +0700

    implement lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import History from './components/History';
-import Login from './components/Login';
-import Register from './components/Register';
-import Setting from './components/Setting';
-import Timer from './components/Timer';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './components/NotFound';
+import { lazy, Suspense } from 'react';
+import Loading from './components/Loading.tsx';
 
 function App() {
+  const Timer = lazy(() => import('./components/Timer.tsx'));
+  const Setting = lazy(() => import('./components/Setting.tsx'));
+  const History = lazy(() => import('./components/History.tsx'));
+  const Login = lazy(() => import('./components/Login.tsx'));
+  const Register = lazy(() => import('./components/Register.tsx'));
+
   return (
     <div className="flex justify-center items-center h-screen text-text">
       <Routes>
-        <Route path="/" element={<Timer />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Timer />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/setting"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Setting />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <Suspense fallback={<Loading />}>
+              <History />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Register />
+            </Suspense>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <p className="text-2xl">Hol on, loading ser...</p>;
+};
+
+export default Loading;
